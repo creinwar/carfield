76a46610754ae85955013f3ca96cdefd434de92e
Mattia Sinigaglia <mattia.sinigaglia5@unibo.it>
2023-08-04 10:41:28 +0200

    connect spatz fetch enable register

--- hw/carfield.sv
+++ hw/carfield.sv
@@ -1487,7 +1487,7 @@ spatz_cluster_wrapper #(
     .meip_i          ( '0 /* Unconnected */ ),
     .msip_i          ( spatzcl_mbox_intr         ),
     .mtip_i          ( spatzcl_timer_intr        ),
-    .debug_req_i     ( '0 /* TODO: connect me */ ),
+    .debug_req_i     ( car_regs_reg2hw.spatz_cluster_fetch_enable  ),
     //AXI Isolate
     .axi_isolate_i         ( slave_isolate_req [FPClusterSlvIdx]   ),
     .axi_isolated_o        ( master_isolated_rsp [FPClusterMstIdx] ),
