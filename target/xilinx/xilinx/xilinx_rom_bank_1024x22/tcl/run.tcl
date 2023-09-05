d7c5144228463efc4a93ee1569bd6e7eaee1a07e
Maicol Ciani <mciani@lagrev4.ee.ethz.ch>
2023-09-05 20:03:19 +0200

    Added comment at beginning fo run.tcl as it was missing

--- target/xilinx/xilinx/xilinx_rom_bank_1024x22/tcl/run.tcl
+++ target/xilinx/xilinx/xilinx_rom_bank_1024x22/tcl/run.tcl
@@ -1,3 +1,9 @@
+# Copyright 2023 ETH Zurich and University of Bologna.
+# Solderpad Hardware License, Version 0.51, see LICENSE for details.
+# SPDX-License-Identifier: SHL-0.51
+#
+# Maicol Ciani <maicol.ciani@unibo.it>
+
 set partNumber $::env(XILINX_PART)
 set boardName  $::env(XILINX_BOARD)
 
