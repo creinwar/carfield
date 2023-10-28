33c1146cd9bdc3300798987df5819421389316e6
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2023-10-28 17:21:39 +0200

    Update non-free commit.

--- carfield.mk
+++ carfield.mk
@@ -118,7 +118,7 @@ endif
 ######################
 
 CAR_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:carfield/carfield-nonfree.git
-CAR_NONFREE_COMMIT ?= 550ca5ba3d7ce7f0fe59501c57728b38aa8924f6
+CAR_NONFREE_COMMIT ?= e98b7fd718aede28047fad83eded006394443352
 
 ## Clone the non-free verification IP for the Carfield TB
 car-nonfree-init:
